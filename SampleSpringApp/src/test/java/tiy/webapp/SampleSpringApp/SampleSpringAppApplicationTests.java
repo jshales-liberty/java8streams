2d4a24be7127433b6190de57f90af60ec2058106
Brian Will <brian.thomas.will@gmail.com>
2017-06-27 10:33:13 -0400

    added SampleSpringApp

---
+++ SampleSpringApp/src/test/java/tiy/webapp/SampleSpringApp/SampleSpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package tiy.webapp.SampleSpringApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleSpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
